9dc9fb1a4ab4ecc8d3998d6e28f99095d8ba0951
LucasPereira9 <lucas970997@gmail.com>
2023-09-02 13:21:01 -0300

    :sparkles: feat: initial commit

---
+++ src/app/Sobre/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from '../page.module.css'
+import Link from 'next/link';
+
+
+export default function Sobre() {
+  return (
+    <main className={styles.main}>
+      <h1>hello woeow</h1>
+      <Link href='/'>lal</Link>
+    </main>
+  )
+}
+
+
+
